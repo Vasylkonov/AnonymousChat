3173651debf93e2cf5e085d83d756ec09487836f
DENys <mainkrafttv59@gmail.com>
2026-07-31 17:35:07 +0200

    fix socket disconnect error

--- server.js
+++ server.js
@@ -7,23 +7,78 @@ const io = require("socket.io")(server);
 
 app.use(express.static("public"));
 
+
+let users = {};
+
+
 io.on("connection", (socket)=>{
 
     console.log("Пользователь подключился");
 
-socket.on("chat message", (data)=>{
-    io.emit("chat message", data);
-});
+
+    socket.on("join", (nickname)=>{
+
+        users[socket.id] = nickname || "Anonymous";
+
+
+        io.emit("users", users);
+
+
+        io.emit("chat message", {
+            nick:"SERVER",
+            text: users[socket.id] + " вошёл в чат"
+        });
+
     });
 
-    socket.on("disconnect", ()=>{
-        console.log("Пользователь вышел");
+
+
+    socket.on("chat message", (data)=>{
+
+        io.emit("chat message", data);
+
     });
+
+
+
+    socket.on("disconnect", ()=>{
+
+
+        let name = users[socket.id];
+
+
+        delete users[socket.id];
+
+
+        io.emit("users", users);
+
+
+        if(name){
+
+            io.emit("chat message", {
+
+                nick:"SERVER",
+                text:name + " вышел из чата"
+
+            });
+
+        }
+
+
+        console.log("Пользователь вышел");
+
+    });
+
+
+});
 
 
 
 const PORT = process.env.PORT || 3000;
 
+
 server.listen(PORT, ()=>{
+
     console.log("Сервер запущен на порту " + PORT);
+
 });
