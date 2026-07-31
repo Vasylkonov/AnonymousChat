2630422abf24850c54787a47ebf4833417d7f722
DENys <mainkrafttv59@gmail.com>
2026-07-31 18:30:00 +0200

    fix script js

--- public/script.js
+++ public/script.js
@@ -1,29 +1,146 @@
 const socket = io();
 
-const input = document.getElementById("message");
+
+let nick = "";
+
+
+
+const login = document.getElementById("login");
+const chat = document.getElementById("chat");
+
+const nickname = document.getElementById("nickname");
+
+const message = document.getElementById("message");
+
 const messages = document.getElementById("messages");
 
 
-function sendMessage(){
 
-    let text = input.value;
 
-    if(text.trim() !== ""){
-        socket.emit("chat message", text);
-        input.value="";
+
+document.getElementById("joinBtn").onclick = function(){
+
+
+    nick = nickname.value.trim();
+
+
+    if(nick === ""){
+        nick = "Anonymous";
     }
 
-}
+
+    socket.emit("join", nick);
 
 
-socket.on("chat message", (msg)=>{
+    login.style.display = "none";
+
+    chat.style.display = "block";
+
+
+};
+
+
+
+
+
+
+document.getElementById("sendBtn").onclick = function(){
+
+
+    let text = message.value.trim();
+
+
+    if(text === ""){
+        return;
+    }
+
+
+
+    socket.emit("chat message", {
+
+        nick:nick,
+
+        text:text
+
+    });
+
+
+
+    message.value="";
+
+
+};
+
+
+
+
+
+
+
+message.addEventListener("keydown", function(e){
+
+
+    if(e.key === "Enter"){
+
+        document.getElementById("sendBtn").click();
+
+    }
+
+
+});
+
+
+
+
+
+
+
+socket.on("chat message", function(data){
+
+
+    addMessage(data);
+
+
+});
+
+
+
+
+
+
+socket.on("old messages", function(data){
+
+
+    data.forEach(addMessage);
+
+
+});
+
+
+
+
+
+
+
+function addMessage(data){
+
 
     let div = document.createElement("div");
 
+
     div.className="message";
 
-    div.textContent=msg;
+
+    div.innerHTML = 
+    "<b>"+data.nick+"</b>: "+data.text;
+
+
 
     messages.appendChild(div);
 
-});
+
+
+    messages.scrollTop = messages.scrollHeight;
+
+
+}
