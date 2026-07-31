71cac23e45051d0fffda49f0b9894fda55c11551
DENys <mainkrafttv59@gmail.com>
2026-07-31 18:25:19 +0200

    new working chat version

--- server.js
+++ server.js
@@ -1,109 +1,134 @@
 const express = require("express");
+
 const app = express();
 
-const http = require("http").createServer(app);
-const { Server } = require("socket.io");
 
-const io = new Server(http);
+const server = require("http").createServer(app);
+
+
+const io = require("socket.io")(server);
+
+
 
 app.use(express.static("public"));
 
+
+
 let users = {};
+
 let messages = [];
 
 
-io.on("connection", (socket)=>{
-
-    console.log("Пользователь подключился");
-
-
-    // отправляем старые сообщения
-    socket.emit("old messages", messages);
 
 
 
-    socket.on("join", (nickname)=>{
-
-        users[socket.id] = nickname || "Anonymous";
+io.on("connection",(socket)=>{
 
 
-        io.emit("users", users);
-
-
-        io.emit("chat message", {
-            nick:"SERVER",
-            text: users[socket.id] + " вошёл в чат"
-        });
-
-    });
+console.log("Пользователь подключился");
 
 
 
-    socket.on("chat message", (data)=>{
-
-
-        if(!data.text) return;
-
-
-        let msg = {
-            nick:data.nick || "Anonymous",
-            text:data.text
-        };
-
-
-        messages.push(msg);
-
-
-        // максимум 100 сообщений
-        if(messages.length > 100){
-            messages.shift();
-        }
-
-
-        io.emit("chat message", msg);
-
-
-    });
+socket.emit("old messages", messages);
 
 
 
-    socket.on("disconnect", ()=>{
 
 
-        let name = users[socket.id];
+socket.on("join",(nickname)=>{
 
 
-        delete users[socket.id];
+users[socket.id]=nickname;
 
 
-        io.emit("users", users);
+io.emit("chat message",{
 
+nick:"SERVER",
 
+text:nickname+" вошёл в чат"
 
-        if(name){
-
-            io.emit("chat message",{
-                nick:"SERVER",
-                text:name+" вышел из чата"
-            });
-
-        }
-
-
-        console.log("Пользователь отключился");
-
-    });
+});
 
 
 });
 
 
 
+
+
+
+socket.on("chat message",(data)=>{
+
+
+messages.push(data);
+
+
+
+if(messages.length>100){
+
+messages.shift();
+
+}
+
+
+
+io.emit("chat message",data);
+
+
+
+});
+
+
+
+
+
+
+socket.on("disconnect",()=>{
+
+
+let name=users[socket.id];
+
+
+delete users[socket.id];
+
+
+
+if(name){
+
+
+io.emit("chat message",{
+
+nick:"SERVER",
+
+text:name+" вышел из чата"
+
+});
+
+
+}
+
+
+
+console.log("Пользователь отключился");
+
+
+});
+
+
+
+});
+
+
+
+
+
 const PORT = process.env.PORT || 3000;
 
 
-http.listen(PORT, ()=>{
+server.listen(PORT,()=>{
+
 
 console.log("Сервер запущен: "+PORT);
- 
-}); 
+
+
+});
